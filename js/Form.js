e4bed568bfaf2baab2ae31d91db86805a7d212f8
MarianaByJus <86327356+MarianaByJus@users.noreply.github.com>
2024-12-04 18:55:59 -0600

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,49 @@
+class Form {
+  constructor() {
+    this.input = createInput("").attribute("placeholder", "Ingresa tu nombre");
+    this.playButton = createButton("jugar");
+    this.titleImg = createImg("./assets/title.png", "Titulo del juego");
+    this.greeting = createElement("h2");
+  }
+
+
+  setElementsPosition() {
+    this.titleImg.position(120,50);
+    this.input.position(width / 2 - 110, height / 2 - 80);
+    this.playButton.position(width / 2 - 90, height / 2 - 20);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
+  }
+
+  setElementsStyle() {
+    this.titleImg.class("gameTitle");
+    this.input.class("customInput");
+    this.playButton.class("customButton");
+    this.greeting.class("greeting");
+  }
+
+  hide() {
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+  }
+  handlemousepresed() {
+  this.playButton.mousePressed (()=>{
+    this.playButton.hide();
+    this.input.hide();
+    var mesage = `Bonjour ${this.input.value()} 👋
+    </br> Esperando jugadores ...🕜`;
+    this.greeting.html(mesage);
+    playerCount += 1;
+    player.name = this.input.value();
+    player.index = playerCount;
+    player.addPlayer();
+    player.updateCount(playerCount);
+  })
+  }
+
+  display() {
+    this.setElementsPosition();
+    this.setElementsStyle();
+    this.handlemousepresed();
+  }
+}
